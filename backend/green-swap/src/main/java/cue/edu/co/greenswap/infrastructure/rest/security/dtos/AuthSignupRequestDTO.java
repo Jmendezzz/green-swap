ed779449bd04117d6db8ad08330b9225f5d4c617
Juan Gerardo <jmendez1025@cue.edu.co>
2024-04-28 23:39:59 -0500

    feat: validations and controller advice implemented

--- backend/green-swap/src/main/java/cue/edu/co/greenswap/infrastructure/rest/security/dtos/AuthSignupRequestDTO.java
+++ backend/green-swap/src/main/java/cue/edu/co/greenswap/infrastructure/rest/security/dtos/AuthSignupRequestDTO.java
@@ -1,10 +1,25 @@
 package cue.edu.co.greenswap.infrastructure.rest.security.dtos;
 
+import cue.edu.co.greenswap.application.constants.UserConstantMessage;
+import cue.edu.co.greenswap.infrastructure.rest.advice.validaton.anotation.ValidPassword;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
 public record AuthSignupRequestDTO (
+
+        @NotBlank(message = UserConstantMessage.NAME_NOT_BLANK)
         String firstName,
+
+        @NotBlank(message = UserConstantMessage.NAME_NOT_BLANK)
         String lastName,
+
+        @Email(message = UserConstantMessage.INVALID_EMAIL)
         String email,
+        @NotBlank(message = UserConstantMessage.PHONE_NUMBER_NOT_BLANK)
         String phoneNumber,
+
+        @NotBlank(message = UserConstantMessage.PASSWORD_NOT_BLANK)
+        @ValidPassword
         String password
 ){
 }
