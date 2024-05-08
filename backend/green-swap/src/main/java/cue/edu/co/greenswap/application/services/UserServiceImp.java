cd505ac790419b2d146ecc1ac3402b90029bc8ef
Juan Gerardo <jmendez1025@cue.edu.co>
2024-05-07 22:46:04 -0500

    docs: user and product services documented

--- backend/green-swap/src/main/java/cue/edu/co/greenswap/application/services/UserServiceImp.java
+++ backend/green-swap/src/main/java/cue/edu/co/greenswap/application/services/UserServiceImp.java
@@ -7,6 +7,7 @@ import cue.edu.co.greenswap.application.ports.usecases.UserService;
 import cue.edu.co.greenswap.domain.dtos.user.CreateUserDTO;
 import cue.edu.co.greenswap.domain.dtos.user.UserDTO;
 import cue.edu.co.greenswap.domain.models.User;
+import cue.edu.co.greenswap.infrastructure.exceptions.UserException;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
@@ -21,6 +22,13 @@ public class UserServiceImp implements UserService {
   private UserConstraint constraint;
   private UserMapperDTO mapper;
 
+
+  /**
+   * Validates if the provided email is already in use by another user.
+   *
+   * @param user the data for the user to be created
+   * @throws UserException if the provided email is already in use with an appropriate error message and HTTP status
+   */
   @Override
   public UserDTO create(CreateUserDTO user) {
     constraint.validateRepeatedEmail(user.email());
@@ -30,15 +38,34 @@ public class UserServiceImp implements UserService {
     return mapper.toDTO(userSaved);
   }
 
+  /**
+   * Retrieves the user with the specified email.
+   *
+   * @param email the email of the user to retrieve
+   * @return an Optional containing the DTO representation of the user if found, or empty if not found
+   */
   @Override
   public Optional<UserDTO> getByEmail(String email) {
     return repository.findByEmail(email).map(mapper::toDTO);
   }
+
+  /**
+   * Verifies the user, updating the verified status to true.
+   *
+   * @param user the user DTO whose verified status is to be updated
+   * @return the DTO representation of the user with the updated verified status
+   */
   @Override
   public UserDTO setVerified(UserDTO user) {
     return mapper.toDTO(repository.setVerified(mapper.toDomain(user)));
   }
 
+  /**
+   * Updates the user information.
+   *
+   * @param user the user DTO containing the updated information
+   * @return the DTO representation of the updated user
+   */
   @Override
   public UserDTO update(UserDTO user) {
     return mapper.toDTO(repository.save(mapper.toDomain(user)));
