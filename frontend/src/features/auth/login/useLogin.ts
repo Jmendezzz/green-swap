f94093b4a249ec374f8e2630be54135cbc31a79d
Juan Gerardo <jmendez1025@cue.edu.co>
2024-05-20 23:32:24 -0500

    fix: auth token 500 error fixed

--- frontend/src/features/auth/login/useLogin.ts
+++ frontend/src/features/auth/login/useLogin.ts
@@ -1,35 +1,34 @@
+import { useUserContext } from '@/context/UserContext';
 import LoginRequestDTO from '@/domain/auth/LoginRequestDTO';
-import { useLocalStorage } from '@/hooks/useLocalStorage';
-import { loginService } from '@/services/authService';
+import { getBasicInfoCurrentUser, loginService } from '@/services/authService';
 import { AxiosError } from 'axios';
 import toast from 'react-hot-toast';
 import { useMutation } from 'react-query';
 import { useNavigate } from 'react-router-dom';
 
-export interface ErrorResponse  {
-      message: string;
-      status: number;
+export interface ErrorResponse {
+  message: string;
+  status: number;
 }
 
 export function useLogin() {
   const navigate = useNavigate();
-  const [setValue] = useLocalStorage(false, 'isAuthenticated');
+  const { setUser } = useUserContext();
 
-  const { mutate,status, error, isSuccess } = useMutation({
+  const { mutate, status, error, isSuccess } = useMutation({
     mutationFn: (loginRequest: LoginRequestDTO) =>
       loginService(loginRequest.email, loginRequest.password),
-    onSuccess: () => {
-      setValue(true);   
-      navigate('/')
+    onSuccess: async () => {
+      const user = await getBasicInfoCurrentUser();
+      setUser(user);
+      navigate('/');
     },
     onError: (error: AxiosError<ErrorResponse>) => {
-      if(error.response?.data){
+      if (error.response?.data) {
         toast.error(error.response.data.message);
-      }
-      else{
+      } else {
         toast.error('Error inesperado, por favor intenta de nuevo');
       }
-      setValue(false);
     },
   });
   return {
--- frontend/src/services/axiosConfig.ts
+++ frontend/src/services/axiosConfig.ts
@@ -1,20 +1,19 @@
-import { useUserContext } from '@/context/UserContext';
+
 import axios from 'axios';
 export const axiosInstace = axios.create({
     baseURL:"http://localhost:8080",
     withCredentials:true
 });
 
-axios.interceptors.response.use(
+axiosInstace.interceptors.response.use(
     function (response) {
       // If the response is successful, just return it
       return response;
     },
-    function (error) {
+    async function (error) {
       // If the response has a status of 401, clear the user state
       if (error.response && error.response.status === 401) {
-        const { setUser } = useUserContext();
-        setUser(null);
+        console.log('Error 401');
       }
   
       return Promise.reject(error);
