330f20cd57f1eac3919ede5155ee4318b5e2a714
Juan Gerardo <jmendez1025@cue.edu.co>
2024-06-04 00:46:45 -0500

    fix: feedback request mapping name

--- backend/green-swap/src/main/java/cue/edu/co/greenswap/infrastructure/rest/controllers/FeedbackController.java
+++ backend/green-swap/src/main/java/cue/edu/co/greenswap/infrastructure/rest/controllers/FeedbackController.java
@@ -8,7 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/exchanges")
+@RequestMapping("/feedbacks")
 @AllArgsConstructor
 public class FeedbackController {
     private final FeedbackService service;
