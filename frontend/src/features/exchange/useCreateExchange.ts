b90c7b6f3f71e4329351c528e690e25f9ee921d9
Juan Gerardo <jmendez1025@cue.edu.co>
2024-06-05 18:58:22 -0500

    feat: reject exchange implemented

--- frontend/src/features/exchange/useCreateExchange.ts
+++ frontend/src/features/exchange/useCreateExchange.ts
@@ -1,6 +1,8 @@
 import { ROUTES } from "@/constants/routes";
 import { CreateExchangeDTO } from "@/domain/exchange/CreateExchangeDTO";
 import { createExchangeService } from "@/services/exchangeService";
+import { CustomError } from "@/utils/customError";
+import { AxiosError } from "axios";
 import toast from "react-hot-toast";
 import { useMutation } from "react-query";
 import { useNavigate } from "react-router-dom";
@@ -13,6 +15,15 @@ function useCreateExchange(){
             onSuccess: () => {
                 toast.success('¡Intercambio creado con éxito!');
                 navigate(ROUTES.myExchanges);
+            },
+            onError:(error:AxiosError<CustomError>) => {
+                if(error.response?.data){
+                    toast.error(error.response.data.message);
+                }else{
+                    toast.error('Ocurrió un error al crear el intercambio');
+                }
+                
+
             }
         }
     );
